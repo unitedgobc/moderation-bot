f764f376cbbd9e71b15db8df7f529d7fcdb5ff30
theGordHoard <electricgord@gmail.com>
2018-10-07 13:12:46 -0600

    Client Configuration

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import ModerationClient from './structures/ModerationClient'
 import * as rethinkdbdash from 'rethinkdbdash'
 import 'source-map-support/register'
 import * as path from 'path'
+import { ClientOptions, CommandClientOptions } from 'eris'
 
 const r = rethinkdbdash({
   optionalRun: false,
@@ -11,7 +12,24 @@ const r = rethinkdbdash({
   }]
 })
 
-let bot = new ModerationClient(process.env.TOKEN)
+let options: ClientOptions = {
+  defaultImageFormat: 'png',
+  autoreconnect: false
+}
+
+let commandOptions: CommandClientOptions = {
+  prefix: process.env.PREFIX.split(','),
+  ignoreBots: true,
+  owner: process.env.OWNER_NAME,
+  defaultCommandOptions: {
+    caseInsensitive: true,
+    permissionMessage: 'You do not have permission to run this command. If you think this is in error please let a moderator or administrator know.',
+    errorMessage: `An unknown error has occured. Please let ${process.env.OWNER_NAME} know.`
+  },
+  description: 'A bot to enable consistent cross-server moderation.'
+}
+
+let bot = new ModerationClient(process.env.TOKEN, options, commandOptions)
 
 bot.loadDirectory(path.join(__dirname, './commands'), r)
 bot.loadDirectory(path.join(__dirname, './events'), r)
