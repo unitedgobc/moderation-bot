60a3627e361273cbcd9a6565d76bdefcd0234338
theGordHoard <electricgord@gmail.com>
2018-10-07 21:51:56 -0600

    Intro to banning

--- src/commands/moderation/bans.ts
+++ src/commands/moderation/bans.ts
@@ -1,9 +1,38 @@
 import ModerationClient from '../../structures/ModerationClient'
 import { ReqlClient } from 'rethinkdbdash/rethinkdbdash'
-import { Message } from 'eris'
+import { Message, TextChannel, Member, User } from 'eris'
+import * as shortid from 'shortid'
+import { ModerationCase } from '../../utils/types'
 
 module.exports = (bot: ModerationClient, r: ReqlClient) => {
   bot.registerCommand('ban', (msg: Message, args: Array<string>) => {
-    console.log('banne')
+    let bannedMember = bot.resolveMember(args[0], (msg.channel as TextChannel).guild)
+    if ((bannedMember instanceof Member)) {
+      if (msg.author.id === (bannedMember as Member).id || bot.memberModifiable(bannedMember as Member, msg.member)) return msg.channel.createMessage('Insufficient Permissions.')
+    }
+
+    let action: ModerationCase = {
+      action: 'ban',
+      moderator: msg.author.id,
+      reason: args.slice(1).join(' '),
+      timestamp: r.now(),
+      user: (bannedMember as Member).id || bannedMember as string
+    }
+
+    let userName: string
+    if ((bannedMember instanceof User) || (bannedMember instanceof Member)) {
+      userName = ((bannedMember as User).username + `#` + (bannedMember as User).discriminator)
+    } else {
+      userName = `<@${bannedMember as string}> (${bannedMember as string})`
+    }
+    msg.channel.createMessage(`Banning ${userName}`)
+
+  }, {
+    guildOnly: true,
+    requirements: {
+      permissions: {
+        banMembers: true
+      }
+    }
   })
 }
--- src/structures/ModerationClient.ts
+++ src/structures/ModerationClient.ts
@@ -1,4 +1,4 @@
-import { CommandClient } from 'eris'
+import { CommandClient, Guild, Member, User } from 'eris'
 import * as path from 'path'
 import * as fs from 'fs'
 import { ReqlClient } from 'rethinkdbdash/rethinkdbdash'
@@ -18,5 +18,61 @@ export default class ModerationClient extends CommandClient {
         this.loadDirectory(path.join(dir, file), r)
       }
     })
+  }
+
+  resolveMember (mention: string, guild: Guild): Member | User | string | null {
+    const matches = mention.match(/^(?:<@!?)?([0-9]+)>?$/)
+    if (matches) return guild.members.get(matches[1]) || this.users.get(matches[1]) || matches[1]
+    const search = mention.toLowerCase()
+
+    const members = guild.members.filter(memberFilterInexact(search))
+    if (members.length === 0) return null
+    if (members.length === 1) return members[0]
+    const exactMembers = members.filter(memberFilterExact(search))
+    if (exactMembers.length === 1) return exactMembers[0]
+
+    const users = this.users.filter(memberFilterInexact(search))
+    if (users.length === 0) return null
+    if (users.length === 1) return users[0]
+    const exactUsers = this.users.filter(memberFilterExact(search))
+    if (exactUsers.length === 1) return exactUsers[0]
+
+    return null
+
+    function memberFilterExact (search) {
+      return mem => mem.user.username.toLowerCase() === search ||
+        (mem.nickname && mem.nickname.toLowerCase() === search) ||
+        `${mem.user.username.toLowerCase()}#${mem.user.discriminator}` === search
+    }
+
+    function memberFilterInexact (search) {
+      return mem => mem.user.username.toLowerCase().includes(search) ||
+        (mem.nickname && mem.nickname.toLowerCase().includes(search)) ||
+        `${mem.user.username.toLowerCase()}#${mem.user.discriminator}`.includes(search)
+    }
+  }
+
+  memberModifiable (member: Member, editor: Member): boolean {
+    let memberRoles = []
+    member.roles.forEach(role => {
+      let guildRole = member.guild.roles.get(role)
+      memberRoles.push(guildRole)
+    })
+    memberRoles.sort((x, y) => {
+      return y.position - x.position
+    })
+
+    let editorRoles = []
+    editor.roles.forEach(role => {
+      let guildRole = editor.guild.roles.get(role)
+      editorRoles.push(guildRole)
+    })
+    editorRoles.sort((x, y) => {
+      return y.position - x.position
+    })
+
+    if (member.guild.ownerID === member.id) return false
+    if (editorRoles[0].position >= memberRoles[0].position) return false
+    else return true
   }
 }
