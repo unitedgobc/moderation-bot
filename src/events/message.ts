a1a41edb9c119fc47b2ae2e551a5b73a7181a5c3
theGordHoard <electricgord@gmail.com>
2018-10-07 21:51:36 -0600

    Increase toxicity sensitivity

--- src/events/message.ts
+++ src/events/message.ts
@@ -7,7 +7,8 @@ module.exports = bot => {
   bot.on('messageCreate', async (msg: Message) => {
     if (msg.author.bot) return
     let res = await perspective.analyze(msg.cleanContent)
-    if (res.attributeScores.TOXICITY.summaryScore.value > 0.7) {
+    if (res.attributeScores.TOXICITY.summaryScore.value > 0.6) {
+      console.log(res.attributeScores.TOXICITY.summaryScore.value + ' ==> ' + msg.content)
       msg.addReaction('⚠')
     }
   })
